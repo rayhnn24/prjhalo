5e41877bbc97dcf02eb2df6c8a002ee6f3868220
rayhnn24 <raihan.kdg23@gmail.com>
2025-05-29 07:31:26 +0800

    Inisialisasi array string untuk menampung 5 nama

--- hallo.cpp
+++ hallo.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-  cout << "poliban";
-  cout << "indonesia"; 
-  return 0;
+    string name [5];
 
+    system("pause");
+    return 0;
 }
