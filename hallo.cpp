10941a58006bb0b7fc0ee119458a79710fadb2b8
rayhnn24 <raihan.kdg23@gmail.com>
2025-05-29 07:41:46 +0800

    memasukan data 5 buah nama  ke dalam array string

--- hallo.cpp
+++ hallo.cpp
@@ -5,6 +5,12 @@ using namespace std;
 int main() {
     string name [5];
 
+    name[0] = "Alice";
+    name[1] = "Bob";
+    name[2] = "Charlie";
+    name[3] = "Diana";
+    name[4] = "Eve";
+
     system("pause");
     return 0;
 }
